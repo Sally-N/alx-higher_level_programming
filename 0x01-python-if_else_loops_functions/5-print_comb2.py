85843e1adc77b19de0ffcc3496c59233a2afb5e3
Sally-N <snzungula@gmail.com>
2023-01-08 04:20:39 +0000

    Numbers should be printed in ascending order, with two digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for number in range(100)
     if number < 10:
-        number_new = str(number).zfill(2)
-            print("{:d,}".format(number_new))
+        print("{:02}".format(number), end=", ")
     else
-        print("{:d,}".format(number))
+        print("{:d}".format(number), end=", ")
