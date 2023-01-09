e66163206bc4a88d7becbb3623693bd6c0f05d51
Sally-N <snzungula@gmail.com>
2023-01-09 12:40:57 +0000

    biggest number in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
-    
+        return None    
     max = my_list[0]
-        for i in range(len(my_list)):
-            if i > max:
-                max = i
 
-        return max
+    for i in my_list:
+        if i > max:
+            max = i
+
+    return max
