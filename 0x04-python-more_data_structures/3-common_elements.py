71e3dc7e6c0779b3d4df497cd05d585bb7adf346
Sally-N <snzungula@gmail.com>
2023-01-12 18:59:26 +0000

    function that returns a set of common elements in two sets.

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-#A set is an unordered collection with no duplicate elements.#
+    #A set is an unordered collection with no duplicate elements.#
     unique_elements = set_1 & set_2
 
     return unique_elements
