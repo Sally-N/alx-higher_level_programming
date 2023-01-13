ab8f0ca9b795367623476144e2a5584b09460515
Sally-N <snzungula@gmail.com>
2023-01-13 19:52:03 +0000

    # tech interview:Create a function def roman_to_int(roman_string): that converts a Roman numeral to an integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    to_sub = 0
-    max_list = max(list_num)
-
-    for n in list_num:
-        if max_list > n:
-            to_sub += n
-
-    return (max_list - to_sub)
 
 
-    def roman_to_int(roman_string):
-        if not roman_string:
-            return 0
+def roman_to_int(roman_string):
+    '''
+    Converts a Roman numeral to an integer
+    '''
+    new_num = 0
+    i = 0
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+                'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
 
-        if not isinstance(roman_string, str):
-            return 0
-
-        rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        list_keys = list(rom_n.keys())
-
-        num = 0
-        last_rom = 0
-        list_num = [0]
-
-        for ch in roman_string:
-            for r_num in list_keys:
-                if r_num == ch:
-                    if rom_n.get(ch) <= last_rom:
-                        num += to_subtract(list_num)
-                        list_num = [rom_n.get(ch)]
-                    else:
-                        list_num.append(rom_n.get(ch))
-
-                    last_rom = rom_n.get(ch)
-
-        num += to_subtract(list_num)
-
-        return (num)
+    if roman_string and type(roman_string) == str:
+        while i < len(roman_string):
+            if i + 1 < len(roman_string) and roman_string[i:i + 2] in romans:
+                new_num += romans[roman_string[i:i + 2]]
+                i += 2
+            else:
+                new_num += romans[roman_string[i]]
+                i += 1
+        return new_num
+    else:
+        return new_num
