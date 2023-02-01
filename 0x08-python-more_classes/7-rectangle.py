aa7d59ad906dc29266462deddabde3881466f497
Sally-N <snzungula@gmail.com>
2023-02-01 09:32:02 +0000

    rectangle exercises

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -96,7 +96,7 @@ class Rectangle:
 
         for i in range(self.__height):
             for j in range(self.__width):
-                shape += '#'
+                shape += str(self.print_symbol)
             if i != self.__height - 1:
                 shape += '\n'
         return shape
