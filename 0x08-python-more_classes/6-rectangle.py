5ead4f4273e98ad0155a9e84121081ed7502fd5c
Sally-N <snzungula@gmail.com>
2023-02-01 09:11:39 +0000

    rectangle exercises

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,9 @@ Module contains Rectangle Class
 
 class Rectangle:
     '''An empty rectangle class'''
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         '''Initialises the Rectangle class
         args:
