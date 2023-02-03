1da783a3d84c26cc11f0228a78b8dda50e14b3e0
Sally-N <snzungula@gmail.com>
2023-02-03 10:57:52 +0000

    Write a function that prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-'''
-    Contains a function that prints a string with first and last name
-    example:
-        My name is <first name> <last name>
-'''
+'''Defines a name-printing function'''
+
 
 def say_my_name(first_name, last_name=""):
     '''
@@ -16,5 +13,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError('first_name must be a string')
     elif not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
-
     print("My name is {} {}".format(first_name, last_name))
