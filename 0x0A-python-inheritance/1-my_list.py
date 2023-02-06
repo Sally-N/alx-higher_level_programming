ed93b7c2d937ab68e6132b1309e3e3be9219497d
Sally-N <snzungula@gmail.com>
2023-02-06 12:25:04 +0000

    python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Module with a class that inherits from another class"""
+"""Module with a class that inherits from MyList class"""
 
 
 class MyList(list):
-    """Prints the list in sorted order"""
+    """A class that inherits from a base class"""
     def print_sorted(self):
-        list.sort()
-    return list
+        print(sorted(self))
