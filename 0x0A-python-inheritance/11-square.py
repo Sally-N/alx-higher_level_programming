3e2d2772aa193d08aea3ef9d68c6e34c217641cc
Sally-N <snzungula@gmail.com>
2023-02-07 04:39:48 +0000

    Python - Inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -25,4 +25,4 @@ class Square(Rectangle):
         return self.__size ** 2
 
     def __str__(self):
-        return (" [{}] {}/{}".format(type(self)).__name__, self.__size, self.__size))
+        return "[Square] {}/{}".format(self.__size, self.__size)
