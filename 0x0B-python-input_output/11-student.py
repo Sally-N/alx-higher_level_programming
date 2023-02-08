6abb854412cf8f37cfd43caa13708769edcab3b5
Sally-N <snzungula@gmail.com>
2023-02-08 22:19:25 +0000

    Python - Input/Output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+"""Defines the Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializer for Student class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of an instance of Student"""
+        new_dict = {}
+        if attrs is not None:
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    new_dict[k] = v
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
