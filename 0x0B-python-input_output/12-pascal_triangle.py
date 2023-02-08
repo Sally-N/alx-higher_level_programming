6f399005b52b4927a9572b4c38024b61bd0fe7f2
Sally-N <snzungula@gmail.com>
2023-02-08 22:36:23 +0000

    Python - Input/Output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""Defines pascal traingle module"""
+
+"""Defines the Pascal Trinagle module"""
 
 
 def pascal_triangle(n):
     """Gets a list of lists of integers representing the Pascal's triangle"""
 
+    output = [[1]]
     if n <= 0:
         return []
 
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
-
-    for i in range(1, n):
-        l[i][0] = 1
-        for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
-            else:
-                l[i][j] = l[i - 1][0]
-    return n
+    while len(output) != n:
+        a = output[-1]
+        tmp = [1]
+        for i in range(len(a) - 1):
+            tmp.append(a[i] + a[i + 1])
+        tmp.append(1)
+        output.append(tmp)
+    return output
