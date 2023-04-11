a8e4b1d354e07ec1ad7f6c17e8af574cfdd5f7af
Sally-N <snzungula@gmail.com>
2023-04-11 19:50:02 +0000

    js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 /* script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer */
 
-if (!parseInt(process.argv[2])) {
-  console.log('Not a number');
+if (parseInt(process.argv[2])) {
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else {
-  console.log('My Number: ' + parseInt(process.argv[2]));
+  console.log('Not a number');
 }
