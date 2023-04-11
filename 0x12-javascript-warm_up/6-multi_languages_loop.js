6811d8f65b1735db5e879869df8435e2aa31dc98
Sally-N <snzungula@gmail.com>
2023-04-11 20:00:18 +0000

    js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -4,6 +4,6 @@
 
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < array.length - 1; i++) {
+for (let i = 0; i <= array.length - 1; i++) {
   console.log(array[i]);
 }
