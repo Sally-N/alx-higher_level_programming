a8f4e100bbd784701cef755c51bb87ed76dcce2e
Sally-N <snzungula@gmail.com>
2023-04-12 09:13:44 +0000

    js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,8 @@
 
 /* script that prints a square */
 
-let string = '';
 if (parseInt(process.argv[2])) {
+  let string = '';
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     for (let j = 0; j < parseInt(process.argv[2]); j++) {
       string += 'X';
