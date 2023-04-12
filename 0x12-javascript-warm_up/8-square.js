ee9f79da2bbf027a53375c8ecebea48c35ba5c58
Sally-N <snzungula@gmail.com>
2023-04-12 09:08:42 +0000

    js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,11 +6,10 @@ let string = '';
 if (parseInt(process.argv[2])) {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     for (let j = 0; j < parseInt(process.argv[2]); j++) {
-      string += 'X';
+      string += 'x';
     }
-    string += '\n';
+    console.log(string);
   }
-  console.log(string);
 } else {
   console.log('Missing size');
 }
