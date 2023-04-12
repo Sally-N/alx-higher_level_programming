f5e218d249f6e4b49f80b28ef1f5c67e527187fc
Sally-N <snzungula@gmail.com>
2023-04-12 10:52:50 +0000

    js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,14 +2,16 @@
 
 /* script that prints a square */
 
-if (parseInt(process.argv[2])) {
-  let string = '';
-  for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    for (let j = 0; j < parseInt(process.argv[2]); j++) {
-      string += 'X';
-    }
-    console.log(string);
-  }
-} else {
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
   console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
 }
