20df8084b4c681a9a0508697af4b65c3705d1529
Sally-N <snzungula@gmail.com>
2023-04-12 10:04:40 +0000

    js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/* script that searches the second biggest integer in the list of arguments */
+
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+  const arr = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    arr.push(parseInt[i]);
+  }
+  arr.sort();
+  arr.reverse();
+  console.log(arr[1]);
+}
