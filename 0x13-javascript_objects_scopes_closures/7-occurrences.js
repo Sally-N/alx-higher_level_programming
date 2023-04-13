30385d17a316a2b212b8d6a1523c5cee969e3069
Sally-N <snzungula@gmail.com>
2023-04-13 08:46:15 +0000

    js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  const words = list.filter(function (element) {
-    return element === searchElement;
-  });
+  const words = list.filter(element => element === searchElement);
   console.log(words.length);
 };
