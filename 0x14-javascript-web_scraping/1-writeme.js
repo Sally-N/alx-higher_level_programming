b2a2c9bc8215e651acd7a18d1541566f7a2ce377
Sally-N <snzungula@gmail.com>
2023-05-24 10:16:47 +0000

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[3], string, function (error) {
+fs.writeFile(process.argv[2], process.argv[3], error => {
   if (error) throw error;
 });	
