f10ff61f7c74f7d052b716a2be9b2b6960be53c2
Sally-N <snzungula@gmail.com>
2023-05-24 11:41:13 +0000

    script that prints the number of movies where the character “Wedge Antilles” is present.

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const charaterId = 18;
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const theMovies = films.filter(film => film.characters.includes('`https://swapi-api.alx-tools.com/api/people/${characterId}/'));
+    const numberOfMovies = theMovies.length;
+    console.log(numberOfMovies);
+  }
+});
